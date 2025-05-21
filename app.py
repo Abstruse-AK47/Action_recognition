6d0a76db008ba26de0108ac8de7f5d0dd53724cd
Abstruse-AK47 <ayan344kapil@gmail.com>
2025-05-22 03:32:41 +0530

    t

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey"  # Required for flash messages
 API_URL = "http://127.0.0.1:8000/recognize_action"
 UPLOAD_FOLDER = "static/uploads"
 
